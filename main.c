6285195f07f5df49b6d6d9fb2ee03000e3889071
ppaksso <113421378+ppaksso@users.noreply.github.com>
2022-09-27 16:35:20 +0900

    02

--- main.c
+++ main.c
@@ -5,18 +5,22 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x,y,z,m;
-	int a,b,c;
-	 x=1;
-	 y=3;z=5;
-	 a=3;
-	 b=4;
-	 c=5;
-	 
-	 y=a*x*x + b*x + c;
-	 m= (x+y+z)/3;
-	 
-	 printf("y=%d,m=%d",y,m);
+	int x,y;
+	
+	printf("input two intergers:"); 
+	scanf("%i %i",&x,&y);
+	
+
+	
+	printf("+ result is %d\n",x+y);
+	printf("- result is %d\n",x-y);	
+	printf("* result is %d\n",x*y);
+	printf("/ result is %d\n",x/y);
+	printf("%% result is %d\n",x%y);
+
+
+
+	
 	 
 	return 0;
 }
